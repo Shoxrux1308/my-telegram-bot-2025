046112b36f8748e32a107625937931fd81715739
Shoxrux1308 <shohruxb1308@gmail.com>
2025-01-18 17:57:46 +0500

    update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from telegram import Bot
 token = os.getenv("TOKEN")
 
 bot = Bot(token)
-# updates = bot.get_updates()
-# print(updates.update_id)
+updates = bot.get_updates()
+print(updates.chat_id)
 # message = bot.send_message(chat_id=6824726862, text="Hello, world!")
-f='C:/Users/user/Desktop/image.png'
-photo = bot.send_photo(chat_id=6824726862, photo=f )
+# f='C:/Users/user/Desktop/image.png'
+# photo = bot.send_photo(chat_id=6824726862, photo=f )
 # print(bot.send_document(chat_id=6824726862, document='README.md'))
 # print(bot.send_audio(chat_id=6824726862, audio='C:/Users/user/Desktop/A2 lesson 6. Ex 2.mp3'))
 # print(bot.send_video(chat_id=6824726862, video='"BAACAgIAAxkBAAOWZrSm8TZXdqnOVtS38kJ-c9XupRwAAu1RAAJosplJg9GyueFNFKg1BA"'))
--- telegram/getUpdates.py
+++ telegram/getUpdates.py
@@ -1,7 +1,8 @@
 class Updates:
     def __init__(self, update_data):
         self.update_id = update_data['result'][-1]['update_id'],
-        self.message = update_data['result'][-1]['message']['text']
+        self.message = update_data['result'][-1]['message']['text'],
+        self.chat_id = update_data['result'][-1]['message']['chat']['id']
     def __str__(self):
         return f"Update ID: {self.update_id}\nMessage: {self.message}"
     
